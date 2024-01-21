017a5929fa59fcb0733c86039cef9e82fac45c12
luis alvarez <luis_01700@hotmail.com>
2024-01-21 09:40:56 -0600

    creacion de metodos y ejecucion de los mismos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ServicioDeMetodos servicioDeMetodos = new ServicioDeMetodos();
+        ArrayList<Integer> calificaciones = new ArrayList<>();
+        double promedio=0;
+        char calificacion='F';
+        String nombre = "";
+
+        nombre = servicioDeMetodos.LeerNombre();
+        calificaciones =  servicioDeMetodos.LeerCalificaciones();
+        promedio = servicioDeMetodos.ObtenerPromedio(calificaciones);
+        calificacion = servicioDeMetodos.ObtenerCalificacion(promedio);
+
+        servicioDeMetodos.ImprimirInformacion(nombre, calificaciones, promedio, calificacion);
     }
 }
---
+++ src/ServicioDeMetodos.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ServicioDeMetodos
+{
+    Scanner leer = new Scanner(System.in);
+    public double ObtenerPromedio(ArrayList<Integer> calificaciones)
+    {
+        double result = 0;
+
+        int suma = calificaciones.stream().mapToInt(item -> item).sum();
+
+        result = suma / 5;
+
+        return  result;
+    }
+
+    public String LeerNombre()
+    {
+        String Nombre = "";
+
+        System.out.println("Dime tu nombre: ");
+        Nombre = leer.nextLine();
+
+        return Nombre;
+    }
+    public ArrayList<Integer> LeerCalificaciones()
+    {
+        ArrayList<Integer> Calificaciones = new ArrayList<Integer>();
+
+        for (int i = 0; i<5; i++)
+        {
+            System.out.println("Dime la calificación " + (i + 1) + " : ");
+            Calificaciones.add(leer.nextInt());
+        }
+
+        return Calificaciones;
+    }
+
+    public char ObtenerCalificacion(double promedio)
+    {
+        char Calificacion = 'F';
+
+        if (promedio >= 51  && promedio <= 60) {
+            Calificacion = 'E';
+        } else if (promedio >= 61 && promedio <= 70) {
+            Calificacion = 'D';
+        } else if (promedio >= 71 && promedio <= 80) {
+            Calificacion = 'C';
+        } else if (promedio >= 81 && promedio <= 90) {
+            Calificacion = 'B';
+        } else if (promedio >= 91) {
+            Calificacion = 'A';
+        }
+
+        return Calificacion;
+    }
+
+    public void ImprimirInformacion(String nombre, ArrayList<Integer> calificaciones, double promedio, char calificacion)
+    {
+        System.out.println("***************************************************");
+        System.out.println("Nombre: " + nombre.toUpperCase());
+        for (int i = 0; i < calificaciones.size(); i++) {
+            System.out.println("Calificación " + (i+1) + " es: " + calificaciones.get(i));
+        }
+        System.out.println("Tu promedio es: " + promedio);
+        System.out.println("Tu Calificación Final es: " + calificacion);
+        System.out.println("***************************************************");
+    }
+}
